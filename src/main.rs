4fe9d6fbd3568c88122cbe2c7adfd720c9af31af
Steven Schulte <spschul@umich.edu>
2017-10-12 03:22:17 -0400

    Added exercise adding, doing, and listing

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,74 @@
 use std::env;
 use std::fs::OpenOptions;
-use std::io::prelude::*;
 use std::path::PathBuf;
+use chrono::prelude::*;
+use std::collections::HashMap;
+
+extern crate serde;
+extern crate serde_json;
+extern crate chrono;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Serialize, Deserialize)]
+struct Exercise {
+    desired: u32,
+    current: u32,
+    created: DateTime<Local>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Regimen {
+    exercises: HashMap<String, Exercise>
+}
+
+impl Regimen {
+    fn add(&mut self, exer: &str, des: &str) {
+        self.exercises.insert(String::from(exer), Exercise { desired: des.parse::<u32>().unwrap(), current: 0, created: Local::now()});
+    }
+
+    fn list(&self) {
+        for (name, details) in &self.exercises {
+            println!("{} {}/{}", name, details.current, details.desired);
+        }
+    }
+
+    fn done(&mut self, exer: &str, des: &str) {
+        self.exercises.get_mut(exer).unwrap().current += des.parse::<u32>().unwrap();
+    }
+}
 
 fn main() {
-    let mut config = PathBuf::from(env::home_dir().unwrap());
-    config.push(".swole");
+    let file = get_file()
+        .expect("Could not open or create file!");
 
-    let mut file = OpenOptions::new()
+    let mut reg: Regimen = serde_json::from_reader(file).unwrap();
+
+    let mut args = env::args();
+    args.next().expect("Only one argument???");
+
+    match args.next().expect("TODO proper error").as_ref() {
+        "add" => reg.add(args.next().expect("Need exercise name").as_ref(), args.next().expect("Need goal number of reps").as_ref()),
+        "list" => reg.list(),
+        "done" => reg.done(args.next().expect("Need exercise name").as_ref(), args.next().expect("Need completed number of reps").as_ref()),
+        _ => println!("Not a valid command")
+    };
+
+    let file = get_file()
+        .expect("Could not open or create file!");
+
+    serde_json::to_writer(file, &reg)
+        .expect("Could not write to file!");
+}
+
+fn get_file () -> std::result::Result<std::fs::File, std::io::Error> {
+    let mut config = PathBuf::from(env::home_dir().unwrap());
+    config.push(".swole.json");
+
+    OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
         .open(config)
-        .expect("Could not open or create file!");
-
-    let mut contents = String::new();
-
-    file.read_to_string(&mut contents)
-        .expect("Something went wrong during the read of the file!");
-
-    println!("Contents: {}", contents);
-
-    let args: Vec<String> =  env::args().collect();
-
-    println!("{:?}", args);
 }
